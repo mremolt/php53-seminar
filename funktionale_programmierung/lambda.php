17b7e9d6150ea3bbfe8d8a500d9aba22b4c26296
Marc Remolt <marc.remolt@googlemail.com>
2010-08-16 11:28:23 +0200

    Abschnitt funktionale Programmierung

---
+++ funktionale_programmierung/lambda.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * Funktionen (es sind hier NICHT! Rückgaben von Funktionsaufrufen gemeint) können
+ * nur in Variablen abgelegt werden. Das bedeutet, sie können nun z.B. als Parameter
+ * an andere Funktionen übergeben werden.
+ *
+ * Um das zu ermöglichen, kann man nun den Funktionsnamen beim Schlüsselwort function
+ * weglassen und erhält dann eine anonyme (lambda) Funktion als Rückgabe. 
+ */
+
+$meine_funktion = function () {
+    return 'Hallo Welt';
+};
+
+echo $meine_funktion();
